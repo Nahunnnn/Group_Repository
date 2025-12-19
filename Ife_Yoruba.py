0469aff150609707854b7ff1914868c69e850434
Ifeeeee <reachifenow@gmail.com>
2025-12-19 17:39:39 +0100

    Ife_Yoruba.py

--- Ife_Yoruba.py
+++ Ife_Yoruba.py
@@ -1,27 +1,28 @@
 #Ife_Yoruba.py
 
 yoruba_dict = {
-    "Bawo": "How",
-    "Ekaaro": "Good Morning",
-    "Ejo": "Please",
-    "Duro": "Wait",
-    "Ope": "Thanks",
-    "Ile": "House",
-    "Omi": "Water",
-    "Onje": "Food",
-    "Baba": "Father",
-    "Iya": "Mother",
-    "Omode": "Child",
-    "Aro": "Soap",
-    "Oja": "Market",
-    "Ona": "Road",
-    "Iwa": "Character",
-    "Efun": "Forgiveness",
-    "Ayo": "Joy",
-    "Ibeji": "Twins",
-    "Ifa": "Oracle",
-    "Odu": "Chapter",
+    "How": "Bawo",
+    "Good Morning": "Ekaaro",
+    "Please": "Ejo",
+    "Wait": "Duro",
+    "Thanks": "Ope",
+    "House": "Ile",
+    "Water": "Omi",
+    "Food": "Onje",
+    "Father": "Baba",
+    "Mother": "Iya",
+    "Child": "Omode",
+    "Soap": "Aro",
+    "Market": "Oja",
+    "Road": "Ona",
+    "Character": "Iwa",
+    "Forgiveness": "Efun",
+    "Joy": "Ayo",
+    "Twins": "Ibeji",
+    "Oracle": "Ifa",
+    "Chapter": "Odu",
 }
 
 def translate(word):
+
     return yoruba_dict.get(word.lower(), "Word not found in Yoruba Dictionary")
