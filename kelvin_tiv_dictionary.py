786226451692290c54ba115cf6a924bb6e67722c
lonely-kel <ketevin11@gmail.com>
2026-01-20 12:51:43 -0800

    Migrate English to Tiv translator to Streamlit

--- kelvin_tiv_dictionary.py
+++ kelvin_tiv_dictionary.py
@@ -1,38 +1,42 @@
-#  English to Tiv Translator
-tiv_dictionary = {
-"helllo":"M sugh",
-"goodbye": "kwase",
-"thank you": "A vo",
-"yes": "Ee",
-"no": "Ao",
-"water": "minger",
-"food": "ishima",
-"man": "or",
-"woman": "mba",
-"child": "wan",
-"house": "ya",
-"road": "tar",
-"sun": "ushar",
-"moon": "ikyo",
-"fire": "kure",
-"love": "hemen",
-"friend": "manger",
-"school": "kwagh u ishim",
-"book": "ityo",
-"money": "kpen"
+import streamlit as st
+
+
+st.title("Tiv Dictionary")
+
+languages = {
+    "hello":"M sugh",
+    "goodbye": "kwase",
+    "thank you": "A vo",
+    "yes": "Ee",
+    "no": "Ao",
+    "water": "minger",
+    "food": "ishima",
+    "man": "or",
+    "woman": "mba",
+    "child": "wan",
+    "house": "ya",
+    "road": "tar",
+    "sun": "ushar",
+    "moon": "ikyo",
+    "fire": "kure",
+    "love": "hemen",
+    "friend": "manger",
+    "school": "kwagh u ishim",
+    "book": "ityo",
+    "money": "kpen"
 }
 
-print("EINGLIH -> TIV TRANSLATOR")
-print("type 'exit' to quit")
-
-while True:
-    word=input("Enter an English word").lower()
-    if word == "exit":
-        print("goodbye")
-        break
-
-    if word in tiv_dictionary:
-        print("TIV translator:",
-              tiv_dictionary[word])
+user_word = st.text_input("Enter a word you would like to translate").strip().lower()
+if  st.button("Translate"):
+    if user_word:
+        #check if the word exists in the dictionary
+        if user_word in languages:
+            translation = languages[user_word]
+            st.success(f"**translation:** {translation}")
+        else:
+            st.warning("sorry,word not in dictionary")
     else:
-        print("sorry,word not in dictionary")
+        st.warning("Please enter a word first")
+
+
+
